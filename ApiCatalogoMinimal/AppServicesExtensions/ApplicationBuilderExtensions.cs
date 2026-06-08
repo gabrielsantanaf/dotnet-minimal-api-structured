6957e30a66e8822466f50ca5eb70d77c60951644
gabrielsantanaf <gabrielsantanaf09@gmail.com>
2026-06-07 23:39:33 -0300

    refactor: Adicionando métodos de extensão de serviços e e contrução de middleware.

---
+++ ApiCatalogoMinimal/AppServicesExtensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace ApiCatalogoMinimal.AppServicesExtensions
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static IApplicationBuilder UseExceptionHandling(this IApplicationBuilder app, IWebHostEnvironment enviroment)
+        {
+            if (enviroment.IsDevelopment()) 
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            return app;
+        }
+
+        public static IApplicationBuilder UseAppCors(this IApplicationBuilder app) 
+        { 
+            app.UseCors(p =>
+            {
+                p.AllowAnyOrigin();
+                p.WithMethods("GET");
+                p.AllowAnyHeader();
+            });
+            return app;
+        }
+
+        public static IApplicationBuilder UseSwaggerMiddleware(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c => { });
+            return app;
+        }
+    }
+}
--- ApiCatalogoMinimal/Program.cs
+++ ApiCatalogoMinimal/Program.cs
@@ -1,79 +1,19 @@
 using ApiCatalogoMinimal.ApiEndpoints;
-using ApiCatalogoMinimal.Context;
-using ApiCatalogoMinimal.Models;
-using ApiCatalogoMinimal.Services;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
-using System.Reflection;
-using System.Reflection.Metadata;
-using System.Text;
+using ApiCatalogoMinimal.AppServicesExtensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-//Configuração do Swagger para enviar o token JWT
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "apiagenda", Version = "v1" });
-
-    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
-    {
-        Name = "Authorization",
-        Type = SecuritySchemeType.ApiKey,
-        Scheme = "Bearer",
-        BearerFormat = "JWT",
-        In = ParameterLocation.Header,
-        Description = "JWT Authorization header",
-    });
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement
-                {
-                    {
-                          new OpenApiSecurityScheme
-                          {
-                              Reference = new OpenApiReference
-                              {
-                                  Type = ReferenceType.SecurityScheme,
-                                  Id = "Bearer"
-                              }
-                          },
-                         new string[] {}
-                    }
-                });
-});
-
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-
-builder.Services.AddDbContext<AppDbContext>(options => 
-                 options.
-                 UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
-
-builder.Services.AddSingleton<ITokenService>(new TokenService());
-
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-                                  .AddJwtBearer(options =>
-                                  {
-                                      options.TokenValidationParameters = new TokenValidationParameters
-                                      {
-                                          ValidateIssuer = true,
-                                          ValidateAudience = true,
-                                          ValidateLifetime = true,
-                                          ValidateIssuerSigningKey = true,
-
-                                          ValidIssuer = builder.Configuration["Jwt:Issuer"],
-                                          ValidAudience = builder.Configuration["Jwt:Audience"],
-                                          IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
-                                      };
-                                  });
-
-builder.Services.AddAuthorization();
+builder.AddApiSwagger();
+builder.AddPersistence();
+builder.Services.AddCors();
+builder.AddAutenticationJwt();
 
 var app = builder.Build();
+
+var environment = app.Environment;
+app.UseExceptionHandling(environment)
+   .UseSwaggerMiddleware()
+   .UseAppCors();
 
 app.UseAuthentication();
 app.UseAuthorization();
@@ -86,12 +26,5 @@ app.MapCategoriaEndpoint();
 
 //-------Endpoints-Produtos---------//
 app.MapProdutoEndpoint();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
 app.Run();
